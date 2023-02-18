66fce3b389d3eca3604bce391b472d28e7957fe2
FoundIzalith <53011549+FoundIzalith@users.noreply.github.com>
2023-02-18 13:35:15 -0500

    Add files via upload

--- mp3ToWav.py
+++ mp3ToWav.py
@@ -1,17 +1,31 @@
 import sys
-from os import path
+from os import path, walk 
 from pydub import AudioSegment
 
 #Convert whole folders of mp3 files into wav 
 
 def main():
-    input_file = argv[1]
-    output_file = argv[2]
+    input_folder = argv[1]
+    output_folder = argv[2]
 
-    if not exists(input_file):
+    if not exists(input_folder):
         print("Error: No input folder")
         exit()
-    if not exists(output_file):
-        output_file = "Output"
+    if not exists(output_folder):
+        output_folder = "Output"
+        os.makedirs(output_folder)
+
+    count = 0
+    files = []
+    for (root, dirnames, filenames) in walk(input_folder):
+        name = Path(root + dirnames)    
+                    
+        for filename in filenames:
+            if filename.endswith('.mp3'):
+                newFileName = filename.stem + '.wav'
+                dest = root + output_folder + newFileName
+                converted = AudioSegment.from_mp3(filename)
+                converted.export(dest, format="wav")
+
 
     
