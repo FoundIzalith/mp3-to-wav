3a16a10d4ff8c848f3233f4f13b23f909be959dc
FoundIzalith <53011549+FoundIzalith@users.noreply.github.com>
2023-02-18 15:38:53 -0500

    runs

--- mp3ToWav.py
+++ mp3ToWav.py
@@ -4,28 +4,25 @@ from pydub import AudioSegment
 
 #Convert whole folders of mp3 files into wav 
 
-def main():
-    input_folder = argv[1]
-    output_folder = argv[2]
+input_folder = sys.argv[1]
+output_folder = sys.argv[2]
 
-    if not exists(input_folder):
-        print("Error: No input folder")
-        exit()
-    if not exists(output_folder):
-        output_folder = "Output"
-        os.makedirs(output_folder)
+if not path.exists(input_folder):
+    print("Error: No input folder")
+    exit()
+if not path.exists(output_folder):
+    output_folder = "Output"
+    os.makedirs(output_folder)
 
-    count = 0
-    files = []
-    for (root, dirnames, filenames) in walk(input_folder):
-        name = Path(root + dirnames)    
-                    
-        for filename in filenames:
-            if filename.endswith('.mp3'):
-                newFileName = filename.stem + '.wav'
-                dest = root + output_folder + newFileName
-                converted = AudioSegment.from_mp3(filename)
-                converted.export(dest, format="wav")
+count = 0
+for (root, dirnames, filenames) in walk(input_folder):                    
+    for filename in filenames:
+        if filename.endswith('.mp3'):
+            newFileName = filename.stem + '.wav'
+            dest = root + output_folder + newFileName
+            converted = AudioSegment.from_mp3(filename)
+            converted.export(dest, format="wav")
+            count = count + 1
 
+print("Converted ", count, " .mp3 files into .wav")
 
-    
